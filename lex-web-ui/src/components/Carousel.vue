e76ce79cb68a58e9a4e1d14a589f19129e019769
matt-amplelabs <43194171+matt-amplelabs@users.noreply.github.com>
2019-04-06 12:53:39 -0400

    updated version in testing
    Just some adj added to fetch info from UI (no meal.json). We can discuss this more.

--- lex-web-ui/src/components/Carousel.vue
+++ lex-web-ui/src/components/Carousel.vue
@@ -1,18 +1,23 @@
 <template>
     <div class="slides">
         <div v-bind:style="{width: innerWidth + 'px'}" class="slides-inner" style="display:-webkit-inline-box">  
-            <div v-for="card in cardData" v-bind:key="card">
-                <div class="card" v-on:click="cardClicked">
-                    <img v-bind:src="card.image" alt="">
-                    <div class="text">
+            <div v-for="card in cardData" v-bind:key="card.name">
+                <div class="card" >
+                    <!-- img v-bind:src="card.image" alt="" -->
+                    <img v-bind:src=responseCard.imageUrl>
+                    <div class="text"
+                      v-if="card.name"
+                    >
                         <h3>{{ card.name }}</h3>
                         <p>{{ card.location }}</p>
                         <a v-bind:href="'tel:' + card.href">{{card.href}}</a>
                         <p><b>Meal time:</b> {{card.time}}</p>
                         <p><b>Who it serves:</b> {{card.serves}}</p>
                     </div>
-                    <button class="directions" v-on:click="goToDirections">
-                        <p class="button"><b>See directions</b></p>
+                    <button class="directions"
+                      v-if="card.name"
+                    >
+                        <p class="button"><b><a v-bind:href="responseCard.attachmentLinkUrl" target="_blank">See directions</a></b></p>
                     </button>
                 </div>
             </div>
@@ -28,39 +33,82 @@
 <script>
 
 export default {
+  name: 'carousel',
+  props: ['responseCard'],
   data() {
     return {
-      cardData: [
-        {
-          name: 'Sistering - Drop-In Centre',
-          location: '962 Bloor St W, Toronto',
-          href: '123-456-7899',
-          time: '9:30-12:30am',
-          serves: 'Everyone',
-          image: 'static/img/flowers.jpeg',
-        },
-      ],
+      // cardData: [
+      //   {
+      //     name: 'Sistering - Drop-In Centre',
+      //     location: '962 Bloor St W, Toronto',
+      //     href: '123-456-7899',
+      //     time: '9:30-12:30am',
+      //     serves: 'Everyone',
+      //     image: 'static/img/flowers.jpeg',
+      //   },
+      // ],
       innerWidth: 0,
       singleWidth: 0,
+      lex: {},
     };
   },
+  beforeMount() {
+    this.lex = Object.assign({}, this.$store.state.lex);
+    console.log('before mounte ...'); // eslint-disable-line
+  },
   computed: {
-    freeMeal() {
-      return this.$store.state.lex.message;
+    // freeMeal() {
+    //   // TODO: just do a regrex on this
+    //   //
+    //   const lexMsg = this.$store.state.lex.message;
+    //   console.log(lexMsg); // eslint-disable-line
+    //   return lexMsg;
+    // },
+    cardData() {
+      // eslint-disable-next-line
+      const regex = /The next free meal closest to you is at (.*?) at (.*?). ##  The meal began at (.*?);/gim;
+      // eslint-disable-next-line
+      const regex2 = /\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})/gim;
+      // eslint-disable-next-line
+      const regex3 = /## this agency serves (.*?). Please/gim;
+      const result = regex.exec(this.lex.message);
+      if (result === null) {
+        return [{
+          name: null,
+          location: null,
+          href: null,
+          time: null,
+          serves: null,
+          images: null,
+        }];
+      }
+      return [{
+        name: result[1] || null,
+        location: result[2] || null,
+        href: regex2.exec(this.lex.message)[0],
+        time: result[3] || null,
+        serves: regex3.exec(this.lex.message)[1],
+        image: '',
+      }];
     },
   },
   methods: {
     goToPrev() {
-      console.log('prev');
+      console.log('prev'); // eslint-disable-line
     },
     goToNext() {
-      console.log('next');
+      console.log('next'); // eslint-disable-line
     },
     cardClicked() {
-      console.log('card clicked');
+      console.log(this.responseCard); // eslint-disable-line
+      const lexMsg = this.$store.state.lex.message;
+      console.log(lexMsg); // eslint-disable-line
+      console.log('card clicked'); // eslint-disable-line
     },
     goToDirections() {
-      console.log('directions');
+      // console.log('directions'); // eslint-disable-line
+      // eslint-disable-next-line
+      return `<p class="button"><b><a href="${this.responseCard.attachmentLinkUrl}" target="_blank">See directions</a></b></p>`;
     },
   },
 };
